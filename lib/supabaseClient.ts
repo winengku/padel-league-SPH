e548cc34c213d6b1f959e38ba116c26801705c37
winengku <winengkuu@gmail.com>
2026-01-14 05:18:35 +0700

    fix: use runtimeconfig for supabase env

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
-  process.env.NUXT_PUBLIC_SUPABASE_URL!,
-  process.env.NUXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+const supabaseUrl = process.env.SUPABASE_URL!;
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY!;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,10 @@ export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss"],
   css: ["~/assets/css/main.css"],
   pages: true,
+  runtimeConfig: {
+    public: {
+      supabaseUrl: process.env.SUPABASE_URL,
+      supabaseAnonKey: process.env.SUPABASE_ANON_KEY,
+    },
+  },
 });
