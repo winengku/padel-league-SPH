a46bf196e052f906b4aff6d34ae7c344fe01c9fc
winengku <winengkuu@gmail.com>
2026-01-14 04:59:17 +0700

    update 1

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  "https://fedukezbnckpkiothcej.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZlZHVrZXpibmNrcGtpb3RoY2VqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgyODc0ODEsImV4cCI6MjA4Mzg2MzQ4MX0.4YXHpCVjVtGKT2sR_fm7fg-I2fPNCNMTqoEpaGULuGY"
+  process.env.NUXT_PUBLIC_SUPABASE_URL!,
+  process.env.NUXT_PUBLIC_SUPABASE_ANON_KEY!
 );
